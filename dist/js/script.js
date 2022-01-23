dcd68886b60942d430ea4d3112666aa1b09974c1
Umid-NT <umidshermatov.muslim@gmail.com>
2022-01-23 12:35:21 +0600

    23_01_2022

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,58 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i)  => {
     lines[i].style.width = item.innerHTML;
 });
+
+function valideForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+              },
+            email: {
+                required: true,
+                email: true
+            },
+            text: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите своё имя!",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            email: {
+              required: "Пожалуйста, введите свою почту!",
+              email: "Неправильно введён адрес почты!"
+            },
+            text: {
+                required: "Пожалуйста, введите ваше сообщение!",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+            }
+          }
+     });
+};
+
+valideForms('#contacts-form');
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    if(!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+});
